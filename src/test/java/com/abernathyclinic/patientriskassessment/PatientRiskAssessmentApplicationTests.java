8cf62dfcf0d68ca1daa823d655c815ed783899c2
davidshin0302 <david.shin0302@gmail.com>
2025-02-25 23:43:15 -0500

    first commit

---
+++ src/test/java/com/abernathyclinic/patientriskassessment/PatientRiskAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abernathyclinic.patientriskassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientRiskAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
