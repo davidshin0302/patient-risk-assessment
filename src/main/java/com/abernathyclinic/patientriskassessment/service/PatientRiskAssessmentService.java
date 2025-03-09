bf3e4d9263452197ac7f212688a2c17ef8253c40
davidshin0302 <david.shin0302@gmail.com>
2025-03-08 23:50:08 -0500

    working on handling 404 case.

--- src/main/java/com/abernathyclinic/patientriskassessment/service/PatientRiskAssessmentService.java
+++ src/main/java/com/abernathyclinic/patientriskassessment/service/PatientRiskAssessmentService.java
@@ -2,10 +2,13 @@ package com.abernathyclinic.patientriskassessment.service;
 
 import com.abernathyclinic.patientriskassessment.dto.clinicrecord.PatientRecordDTO;
 import com.abernathyclinic.patientriskassessment.dto.patientdemographic.PatientListDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Service
 public class PatientRiskAssessmentService {
     @Autowired
@@ -13,13 +16,25 @@ public class PatientRiskAssessmentService {
     @Autowired
     private PatientRecordClient patientRecordClient;
 
-    public boolean findPatientById(String patId){
-
+    public Mono<PatientRecordDTO> getPatientRecord(String patId) {
+        return patientRecordClient.fetchPatientRecords(patId)
+                .onErrorResume(WebClientResponseException.class, ex -> {
+                    if (ex.getStatusCode().value() == 404) {
+                        log.warn("Unable to find Patient Record by patId: {}", patId);
+                        return Mono.empty();
+                    } else {
+                        log.error("Error fetching patient record: {}", patId);
+                        return Mono.error(new RuntimeException("Error at Runtime"));
+                    }
+                });
     }
 
     public void getPatientRiskAssessment(String patId) {
+        Mono<PatientRecordDTO> patientRecordsDTOMono = getPatientRecord(patId);
+
+        //I want to implement to return if getPaitentRecord is empty because I know there are no data so no point of continuing below logic.
+
         Mono<PatientListDTO> patientListDTOMono = patientDemoGraphicsApiClient.fetchPatientDemoGraphicData();
-        Mono<PatientRecordDTO> patientRecordsDTOMono = patientRecordClient.fetchPatientRecords(patId);
 
         Mono.zip(patientListDTOMono, patientRecordsDTOMono).map(tuple -> {
                     PatientListDTO patientListDTO = tuple.getT1();
