c3bd8452dd96423dae64fced7854acfa448ea4d3
davidshin0302 <david.shin0302@gmail.com>
2025-03-12 23:51:30 -0400

    extract patient lastname from the note.

--- src/main/java/com/abernathyclinic/patientriskassessment/service/PatientRiskAssessmentService.java
+++ src/main/java/com/abernathyclinic/patientriskassessment/service/PatientRiskAssessmentService.java
@@ -9,8 +9,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
 
-import java.util.List;
-
 @Slf4j
 @Service
 public class PatientRiskAssessmentService {
@@ -31,11 +29,32 @@ public class PatientRiskAssessmentService {
                 });
     }
 
-    public void getPatientRiskAssessment(String patId) {
+    public String extractPatientName(String note) {
+        String prefix = "patient: ";
+
+        int startIndex = note.toLowerCase().indexOf(prefix);
+        int endIndex;
+
+        if (startIndex != -1) {
+            startIndex += prefix.length();
+            endIndex = note.indexOf(" ", startIndex);
+
+            if (endIndex != -1) {
+                return note.substring(startIndex, endIndex);
+            }
+        }
+        return null;
+    }
+
+    public Mono<PatientDiabetesAssessment> getPatientRiskAssessment(String patId) {
         return getPatientRecord(patId)
                 .flatMap(patientRecordDTO -> {
                     Mono<PatientListDTO> patientListDTO = patientDemographicsApiClient.fetchPatientDemoGraphicData();
 
+                    return Mono.zip(patientListDTO, Mono.just(patientRecordDTO), (tuple1, tuple2) -> {
+
+                        return
+                    })
                 })
     }
 }
