df1807a85894d8ce40ed402ceb89ecf2bd4d1677
github username <github email address>
2025-03-18 11:29:23 -0400

    Implementing edge test cases for PatientRecordClientTest class.

---
+++ src/test/java/com/abernathyclinic/patientriskassessment/service/PatientRecordClientTest.java
@@ -0,0 +1,70 @@
+package com.abernathyclinic.patientriskassessment.service;
+
+import com.abernathyclinic.patientriskassessment.dto.clinicrecord.PatientRecordDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class PatientRecordClientTest {
+    PatientRecordClient patientRecordClient;
+
+    ObjectMapper objectMapper;
+
+    MockWebServer mockWebServer;
+
+    PatientRecordDTO patientRecordDTO;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        mockWebServer = new MockWebServer();
+        mockWebServer.start();
+
+        String FILE_PATH = "src/test/resources/mockPatientRecordData.json";
+        objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+
+        patientRecordDTO = objectMapper.readValue(new File(FILE_PATH), PatientRecordDTO.class);
+
+        String patId= "1";
+        String url = mockWebServer.url("/patHistory/get?patId=" + patId).toString();
+        patientRecordClient = new PatientRecordClient(WebClient.builder(), url);
+    }
+
+    @AfterEach
+    void tearDwon() throws IOException {
+        mockWebServer.shutdown();
+    }
+
+    @Test
+    void fetchPatientRecords() throws JsonProcessingException {
+        mockWebServer.enqueue(new MockResponse()
+                .setResponseCode(HttpStatus.OK.value())
+                .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .setBody(objectMapper.writeValueAsString(patientRecordDTO)));
+
+        String patId = "1";
+        Mono<PatientRecordDTO> output = patientRecordClient.fetchPatientRecords(patId);
+        StepVerifier.create(output)
+                .expectNextMatches(patientRecrod -> {
+                    assertNotNull(patientRecrod);
+                    return true;
+                })
+                .verifyComplete();
+    }
+}
