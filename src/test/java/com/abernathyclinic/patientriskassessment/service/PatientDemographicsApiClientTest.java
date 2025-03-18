0a225535c0d99e696337eef22201d4454e44e434
github username <github email address>
2025-03-18 11:06:51 -0400

    Completed setting up edge cases for PatientDemographicsApiClientTest class.

--- src/test/java/com/abernathyclinic/patientriskassessment/service/PatientDemographicsApiClientTest.java
+++ src/test/java/com/abernathyclinic/patientriskassessment/service/PatientDemographicsApiClientTest.java
@@ -3,12 +3,15 @@ package com.abernathyclinic.patientriskassessment.service;
 import com.abernathyclinic.patientriskassessment.dto.patientdemographic.PatientListDTO;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
@@ -17,6 +20,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Objects;
 
+@Slf4j
 class PatientDemographicsApiClientTest {
     PatientDemographicsApiClient patientDemographicsApiClient;
 
@@ -50,13 +54,31 @@ class PatientDemographicsApiClientTest {
     void fetchPatientDemoGraphicData() throws JsonProcessingException {
         mockWebServer.enqueue(new MockResponse()
                 .setResponseCode(HttpStatus.OK.value())
-                .setHeader("Content-Type", "application/json")
+                .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
                 .setBody(objectMapper.writeValueAsString(patientListDTO)));
 
         Mono<PatientListDTO> output = patientDemographicsApiClient.fetchPatientDemoGraphicData();
 
         StepVerifier.create(output)
                 .expectNextMatches(Objects::nonNull)
+                .verifyComplete();
+    }
+
+    @Test
+    void fetchPatientDemoGraphicData_NotFound() {
+        mockWebServer.enqueue(new MockResponse()
+                .setResponseCode(HttpStatus.NOT_FOUND.value()));
+
+        StepVerifier.create(patientDemographicsApiClient.fetchPatientDemoGraphicData())
+                .verifyComplete();
+    }
+
+    @Test
+    void fetchPatientDemoGrpahicData_InternalError() {
+        mockWebServer.enqueue(new MockResponse()
+                .setResponseCode(HttpStatus.INTERNAL_SERVER_ERROR.value()));
+
+        StepVerifier.create(patientDemographicsApiClient.fetchPatientDemoGraphicData())
                 .verifyComplete();
     }
 }
